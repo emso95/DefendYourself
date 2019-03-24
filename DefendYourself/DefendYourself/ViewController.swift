2a63623217dfc3c4b24f041a38bef5d5f3598b17
Emir Can Marangoz <emircanmarangoz@emirs-macbook-pro.local>
2019-03-23 23:59:07 -0400

    first

--- DefendYourself/DefendYourself/ViewController.swift
+++ DefendYourself/DefendYourself/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    @IBAction func onIceBall(_ sender: Any) {
+        fireMissile()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,12 +27,60 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
     }
+    func getUserVector() -> (SCNVector3, SCNVector3) { // (direction, position)
+        if let frame = self.sceneView.session.currentFrame {
+            let mat = SCNMatrix4(frame.camera.transform) // 4x4 transform matrix describing camera in world space
+            let dir = SCNVector3(-1 * mat.m31, -1 * mat.m32, -1 * mat.m33) // orientation of camera in world space
+            let pos = SCNVector3(mat.m41, mat.m42, mat.m43) // location of camera in world space
+            
+            return (dir, pos)
+        }
+        return (SCNVector3(0, 0, -1), SCNVector3(0, 0, -0.2))
+    }
+    func fireMissile(){
+        var node = SCNNode()
+        //create node
+        node = createMissile()
+        
+        //get the users position and direction
+        let (direction, position) = self.getUserVector()
+        node.position = position
+        var nodeDirection = SCNVector3()
+        
+        nodeDirection  = SCNVector3(direction.x*4,direction.y*4,direction.z*4)
+        node.physicsBody?.applyForce(nodeDirection, at: SCNVector3(0.1,0,0), asImpulse: true)
+            
+       
+        
+        //move node
+        node.physicsBody?.applyForce(nodeDirection , asImpulse: true)
+        
+        //add node to scene
+        sceneView.scene.rootNode.addChildNode(node)
+    }
     
+    //creates nodes
+    func createMissile()->SCNNode{
+        
+        let sphere = SCNSphere(radius: 0.03)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "ice.jpg")
+        sphere.materials = [material]
+        let sphereNode = SCNNode(geometry: sphere)
+        
+        sphereNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        sphereNode.physicsBody?.isAffectedByGravity = false
+        
+        //these bitmasks used to define "collisions" with other objects
+        //sphereNode.physicsBody?.categoryBitMask = CollisionCategory.missileCategory.rawValue
+        //sphereNode.physicsBody?.collisionBitMask = CollisionCategory.targetCategory.rawValue
+        return sphereNode
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
