403149d39352371db31957be9d7cad982e7a126c
Emir Can Marangoz <emircanmarangoz@emirs-macbook-pro.local>
2019-03-24 22:47:57 -0400

    ball design

--- DefendYourself/DefendYourself/ViewController.swift
+++ DefendYourself/DefendYourself/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -22,15 +22,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-        
+        sceneView.scene.physicsWorld.contactDelegate = self
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene()
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+       
+        addTargetNodes()
     }
     func getUserVector() -> (SCNVector3, SCNVector3) { // (direction, position)
         if let frame = self.sceneView.session.currentFrame {
@@ -77,10 +75,43 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sphereNode.physicsBody?.isAffectedByGravity = false
         
         //these bitmasks used to define "collisions" with other objects
-        //sphereNode.physicsBody?.categoryBitMask = CollisionCategory.missileCategory.rawValue
-        //sphereNode.physicsBody?.collisionBitMask = CollisionCategory.targetCategory.rawValue
+        sphereNode.physicsBody?.categoryBitMask = CollisionCategory.missileCategory.rawValue
+        sphereNode.physicsBody?.collisionBitMask = CollisionCategory.targetCategory.rawValue
         return sphereNode
     }
+    func addTargetNodes(){
+        let (direction, position) = self.getUserVector()
+        for index in 1...5 {
+            
+            
+            let sphere = SCNSphere(radius: 1)
+            let material = SCNMaterial()
+            material.diffuse.contents = UIImage(named: "fire.jpg")
+            sphere.materials = [material]
+            let sphereNode = SCNNode(geometry: sphere)
+            
+            
+            
+            
+            sphereNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+            sphereNode.physicsBody?.isAffectedByGravity = false
+            //place randomly, within thresholds
+            sphereNode.position = SCNVector3(Float.random(in: -10 ... 10),Float.random(in: -4 ... 5),Float.random(in: -40 ... -10))
+            
+            
+            //rotate
+            let action : SCNAction = SCNAction.move(to: position, duration: 10)
+             sphereNode.runAction(action)
+            
+            //for the collision detection
+            sphereNode.physicsBody?.categoryBitMask = CollisionCategory.targetCategory.rawValue
+            sphereNode.physicsBody?.contactTestBitMask = CollisionCategory.missileCategory.rawValue
+            
+            //add to scene
+            sceneView.scene.rootNode.addChildNode(sphereNode)
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -123,4 +154,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        
+        
+        if contact.nodeA.physicsBody?.categoryBitMask == CollisionCategory.targetCategory.rawValue
+            || contact.nodeB.physicsBody?.categoryBitMask == CollisionCategory.targetCategory.rawValue {
+            
+            
+            DispatchQueue.main.async {
+                contact.nodeA.removeFromParentNode()
+                contact.nodeB.removeFromParentNode()
+                
+            }
+            
+            //playSound(sound: "explosion", format: "wav")
+            let  explosion = SCNParticleSystem(named: "Explode", inDirectory: nil)
+            contact.nodeB.addParticleSystem(explosion!)
+        }
+    }
+}
+struct CollisionCategory: OptionSet {
+    let rawValue: Int
+    static let missileCategory  = CollisionCategory(rawValue: 1 << 0)
+    static let targetCategory = CollisionCategory(rawValue: 1 << 1)
 }
